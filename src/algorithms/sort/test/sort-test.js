b0aa40da99fa001eaf28b5c51fa0bcf5db3195a8
Vanderci <vandercijr@gmail.com>
2020-05-23 11:39:32 -0300

    filled test variables used as input parameters

--- src/algorithms/sort/test/sort-test.js
+++ src/algorithms/sort/test/sort-test.js
@@ -16,11 +16,45 @@ const { bubbleSort } = require('../sort');
 const  expect = chai.expect;
 
 //names generated from ttps://www.name-generator.org.uk
-const  array_input = [];
+//names generated from ttps://www.name-generator.org.uk
+const  array_input = [
+  'Paris Cutler',
+  'Gideon Stibbons',
+  'Cordelia Holt',
+  'Randall Ridcully',
+  'Nadia Drake',
+  'Ruki Imperial',
+  'Tamika Konstan',
+  'Faust Rune',
+  'Ponder Weasley',
+  'Ommin Voldemort'
+];
 
-const  asorted_array = [];
+const  asorted_array = [
+  'Cordelia Holt',
+  'Faust Rune',
+  'Gideon Stibbons',
+  'Nadia Drake',
+  'Ommin Voldemort',
+  'Paris Cutler',
+  'Ponder Weasley',
+  'Randall Ridcully',
+  'Ruki Imperial',
+  'Tamika Konstan'
+];
 
-const  dsorted_array = [];
+const  dsorted_array = [
+  'Tamika Konstan',
+  'Ruki Imperial',
+  'Randall Ridcully',
+  'Ponder Weasley',
+  'Paris Cutler',
+  'Ommin Voldemort',
+  'Nadia Drake',
+  'Gideon Stibbons',
+  'Faust Rune',
+  'Cordelia Holt'
+];
 
 describe('Test some sort algorithms', ()  =>  {
   it('Should sort an array using ascendent BubbleSort algorithm', ()  =>  {
