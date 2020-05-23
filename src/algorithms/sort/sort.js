72babff394c7674ec15169705d798ee103a5a633
Vanderci <vandercijr@gmail.com>
2020-05-23 11:54:49 -0300

    added bubble sort function

--- src/algorithms/sort/sort.js
+++ src/algorithms/sort/sort.js
@@ -6,3 +6,29 @@
  * Data: 23/05/2020
  *
  */
+ const bubbleSort =  (array_input, order)  =>  {
+   const asize = array_input.length;
+
+   if (asize > 2) {
+     let found = true;
+
+     while (found) {
+       found = false;
+
+       for (let i = 0; i < asize; i++) {
+         const a = order === 'desc' ? i + 1 : i,
+               b = order === 'desc' ? i : i + 1;
+
+         const shouldSwap = array_input[a] > array_input[b];
+
+         if (shouldSwap) {
+           swap(array_input, a, b);
+         }
+       }
+     }
+   }
+ };
+
+ module.exports = {
+   bubbleSort: bubbleSort
+ };
