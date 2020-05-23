337efe0ba176ef90ab564c21f935fe077441f46d
Vanderci <vandercijr@gmail.com>
2020-05-23 10:16:11 -0300

    added sort algol tests

---
+++ src/algorithms/sort/test/sort-test.js
@@ -0,0 +1,16 @@
+'use strict';
+/**
+ * Arquivo: sort-test.js
+ * Author: Vanderci Curvelo Junior
+ * Description: test for the file : 'sort.js'
+ * Data: 23/05/2020
+ *
+ * Documentação: http://chaijs.com/guide/styles/#expect
+ *
+ */
+
+const  chai = require('chai');
+
+const { bubbleSort } = require('../sort');
+
+const  expect = chai.expect;
