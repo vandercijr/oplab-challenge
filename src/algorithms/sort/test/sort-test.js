b3e30abda9dfc048d877d0d4f11bb7013cf4eed6
Vanderci <vandercijr@gmail.com>
2020-05-23 10:17:38 -0300

    added sort algol tests descriptors

--- src/algorithms/sort/test/sort-test.js
+++ src/algorithms/sort/test/sort-test.js
@@ -14,3 +14,22 @@ const  chai = require('chai');
 const { bubbleSort } = require('../sort');
 
 const  expect = chai.expect;
+
+//names generated from ttps://www.name-generator.org.uk
+const  array_input = [];
+
+const  asorted_array = [];
+
+const  dsorted_array = [];
+
+describe('Test some sort algorithms', ()  =>  {
+  it('Should sort an array using ascendent BubbleSort algorithm', ()  =>  {
+    bubbleSort(array_input);
+    expect(array_input).to.deep.equal(asorted_array);
+  });
+
+  it('Should sort an array using descendent BubbleSort algorithm', () =>  {
+    bubbleSort(array_input, 'desc');
+    expect(array_input).to.deep.equal(dsorted_array);
+  });
+});
