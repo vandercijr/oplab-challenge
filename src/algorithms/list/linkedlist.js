c2c048d4cb87805615df8a22f076da816f3bdb34
Vanderci <vandercijr@gmail.com>
2020-05-24 08:23:01 -0300

    added linked list implementation

---
+++ src/algorithms/list/linkedlist.js
@@ -0,0 +1,11 @@
+'use strict';
+/**
+ * Arquivo: linkedlist.js
+ * Author: Vanderci Curvelo Junior
+ * Description: very basic implementation of some linkedlist algorithms'
+ * Data: 24/05/2020
+ *
+ */
+const SinglyLinkedList = require('./singlylinkedlist');
+
+module.exports = SinglyLinkedList;
