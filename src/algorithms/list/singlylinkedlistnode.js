803a75e402caa609a35fa7f762d5067f98055680
Vanderci <vandercijr@gmail.com>
2020-05-24 08:24:28 -0300

    added singly linked list node implementation

---
+++ src/algorithms/list/singlylinkedlistnode.js
@@ -0,0 +1,36 @@
+'use strict';
+
+ //////////////////////////////////////////////////////////////////////////
+/**
+ * Node structure
+ */
+class Node {
+  constructor(id, data, next) {
+    this._id = id;
+    this._data = data;
+    this._next = next;
+  }
+
+  set data(value) {
+    this._data = value;
+  }
+
+  get data() {
+    return this._data;
+  }
+
+  set next(value) {
+    this._next = value;
+  }
+
+  get next() {
+    return this._next;
+  }
+
+  get id() {
+    return this._id;
+  }
+}
+//////////////////////////////////////////////////////////////////////////
+
+module.exports = Node;
