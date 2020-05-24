06661dc0cced80c8fb139c0955492939697ef8fc
Vanderci <vandercijr@gmail.com>
2020-05-24 08:21:49 -0300

    added create linked list test

--- src/algorithms/list/test/linkedlist-test.js
+++ src/algorithms/list/test/linkedlist-test.js
@@ -11,4 +11,24 @@
  */
 
  const chai = require('chai');
+ const SinglyLinkedList = require('../linkedlist');
+ const Node = require('../singlylinkedlistnode');
  const expect = chai.expect;
+
+ const input = ['Name 1','Name 2'];
+ //names generated from ttps://www.name-generator.org.uk
+
+ const item1 = new Node(1, input[0]);
+ const item2 = new Node(2, input[1]);
+
+ const mylist = new Node(item1._id, item1._data);
+
+ mylist.next = item2;
+
+ describe('Test some linkedlist algorithms', () => {
+   it('Should have count equal to 0 on creating a linkedlist', () => {
+     const singlyLinkedList = new SinglyLinkedList();
+
+     expect(singlyLinkedList.count()).to.be.equal(0);
+ 	});
+});
