0a9ce06682dbd896f3a654627ac1cd7f931a2cbb
Vanderci <vandercijr@gmail.com>
2020-05-24 08:13:48 -0300

    added list folder

---
+++ src/algorithms/list/test/linkedlist-test.js
@@ -0,0 +1,11 @@
+'use strict'
+
+/**
+ * Arquivo: linkedlist-test.js
+ * Author: Vanderci Curvelo Junior
+ * Description: test for the file : 'linkedlist.js'
+ * Data: 24/05/2020
+ *
+ * Documentação: http://chaijs.com/guide/styles/#expect
+ *
+ */
