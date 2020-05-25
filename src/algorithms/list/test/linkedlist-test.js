4e716f2fdec20e0263f7a4f587506b37e104a46b
Vanderci <vandercijr@gmail.com>
2020-05-25 14:16:54 -0300

    eslint fixes

--- src/algorithms/list/test/linkedlist-test.js
+++ src/algorithms/list/test/linkedlist-test.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 /**
  * Arquivo: linkedlist-test.js
@@ -10,46 +10,45 @@
  *
  */
 
- const chai = require('chai');
- const SinglyLinkedList = require('../linkedlist');
- const Node = require('../singlylinkedlistnode');
- const expect = chai.expect;
+const chai = require('chai');
+const SinglyLinkedList = require('../linkedlist');
+const Node = require('../singlylinkedlistnode');
+const expect = chai.expect;
 
- const input = ['Name 1','Name 2'];
- //names generated from ttps://www.name-generator.org.uk
+const input = ['Name 1','Name 2'];
 
- const item1 = new Node(1, input[0]);
- const item2 = new Node(2, input[1]);
+const item1 = new Node(1, input[0]);
+const item2 = new Node(2, input[1]);
 
- const mylist = new Node(item1._id, item1._data);
+const mylist = new Node(item1._id, item1._data);
 
- mylist.next = item2;
+mylist.next = item2;
 
- describe('Test some linkedlist algorithms', () => {
-   it('Should have count equal to 0 on creating a linkedlist', () => {
-     const singlyLinkedList = new SinglyLinkedList();
+describe('Test some linkedlist algorithms', function() {
+  it('Should have count equal to 0 on creating a linkedlist', function() {
+    const singlyLinkedList = new SinglyLinkedList();
 
-     expect(singlyLinkedList.count()).to.be.equal(0);
- 	});
+    expect(singlyLinkedList.count()).to.be.equal(0);
+  });
 
-  it('Should have count equal to 1 pushing a item to the linked list', () => {
+  it('Should have count equal to 1 pushing a item to the linked list', function() {
     const singlyLinkedList = new SinglyLinkedList();
 
     singlyLinkedList.push(1, input[0]);
 
     expect(singlyLinkedList.count()).to.be.equal(1);
-	});
+  });
 
-  it('Should have count equal to 2 pushing a item to the linked list', () => {
+  it('Should have count equal to 2 pushing a item to the linked list', function() {
     const singlyLinkedList = new SinglyLinkedList();
 
     singlyLinkedList.push(1, input[0]);
     singlyLinkedList.push(2, input[1]);
 
     expect(singlyLinkedList.count()).to.be.equal(2);
-	});
+  });
 
-  it('Should push an item to the linked list', () => {
+  it('Should push an item to the linked list', function() {
     const singlyLinkedList = new SinglyLinkedList();
 
     singlyLinkedList.push(1, input[0]);
@@ -57,7 +56,7 @@
     expect(singlyLinkedList.findAll()).to.deep.equal(item1);
   });
 
-  it('Should push two items to the linked list', () => {
+  it('Should push two items to the linked list', function() {
     const singlyLinkedList = new SinglyLinkedList();
 
     singlyLinkedList.push(1, input[0]);
@@ -66,34 +65,34 @@
     expect(singlyLinkedList.findAll()).to.deep.equal(mylist);
   });
 
-  it('Should find the specified item on the linked list', () => {
+  it('Should find the specified item on the linked list', function() {
     const singlyLinkedList = new SinglyLinkedList();
 
     singlyLinkedList.push(1, input[0]);
     singlyLinkedList.push(2, input[1]);
 
     expect(singlyLinkedList.find(2)).to.deep.equal(item2);
-	});
+  });
 
-  it('Should remove the first item from the linked list', () => {
+  it('Should remove the first item from the linked list', function() {
     const singlyLinkedList = new SinglyLinkedList();
 
     singlyLinkedList.push(1, input[0]);
     singlyLinkedList.push(2, input[1]);
 
-    singlyLinkedList.remove(1)
+    singlyLinkedList.remove(1);
 
     expect(singlyLinkedList.findAll()).to.deep.equal(item2);
-	});
+  });
 
-  it('Should remove the second item from the linked list', () => {
+  it('Should remove the second item from the linked list', function() {
     const singlyLinkedList = new SinglyLinkedList();
 
     singlyLinkedList.push(1, input[0]);
     singlyLinkedList.push(2, input[1]);
 
-    singlyLinkedList.remove(2)
+    singlyLinkedList.remove(2);
 
     expect(singlyLinkedList.findAll()).to.deep.equal(item1);
-	});
+  });
 });
