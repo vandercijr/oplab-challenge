d11d0ff66f56183c9536760bb197db6b37e0a28a
Vanderci <vandercijr@gmail.com>
2020-05-25 14:17:11 -0300

    eslint fixes

--- src/algorithms/sort/sort.js
+++ src/algorithms/sort/sort.js
@@ -6,37 +6,37 @@
  * Data: 23/05/2020
  *
  */
- const swap = (swap_array, i, j) =>  {
-   const auxiliar = swap_array[i];
+const swap = (swap_array, i, j) =>  {
+  const auxiliar = swap_array[i];
 
-   swap_array[i] = swap_array[j];
-   swap_array[j] = auxiliar;
- };
+  swap_array[i] = swap_array[j];
+  swap_array[j] = auxiliar;
+};
 
- const bubbleSort =  (array_input, order)  =>  {
-   const asize = array_input.length;
+const bubbleSort =  (array_input, order)  =>  {
+  const asize = array_input.length;
 
-   if (asize > 2) {
-     let found = true;
+  if (asize > 2) {
+    let found = true;
 
-     while (found) {
-       found = false;
+    while (found) {
+      found = false;
 
-       for (let i = 0; i < asize; i++) {
-         const a = order === 'desc' ? i + 1 : i,
-               b = order === 'desc' ? i : i + 1;
+      for (let i = 0; i < asize; i++) {
+        const a = order === 'desc' ? i + 1 : i,
+          b = order === 'desc' ? i : i + 1;
 
-         const shouldSwap = array_input[a] > array_input[b];
+        const shouldSwap = array_input[a] > array_input[b];
 
-         if (shouldSwap) {
-           found = true;
-           swap(array_input, a, b);
-         }
-       }
-     }
-   }
- };
+        if (shouldSwap) {
+          found = true;
+          swap(array_input, a, b);
+        }
+      }
+    }
+  }
+};
 
- module.exports = {
-   bubbleSort: bubbleSort
- };
+module.exports = {
+  bubbleSort: bubbleSort
+};
