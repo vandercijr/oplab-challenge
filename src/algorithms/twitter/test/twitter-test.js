bdb8b844f3d773d0b901bfe7a24861c9fdeaf8ba
Vanderci <vandercijr@gmail.com>
2020-05-31 17:15:50 -0300

    fixed response function

--- src/algorithms/twitter/test/twitter-test.js
+++ src/algorithms/twitter/test/twitter-test.js
@@ -12,14 +12,34 @@
 const chai = require('chai');
 
 const Twitter = require('../twitter');
+const path  = require('path');
+const dotenv  = require('dotenv');
+
+dotenv.config({path: path.resolve(__dirname + '/../../../../.env') });
 
 const expect = chai.expect;
 
 describe('Test tweets from twitter Api', () => {
   it('Give a string search five tweets with this word', () =>  {
-    const twitter = Twitter("Development");
-    twitter.events.on('tweet', function (e) {
-      expect(e.tweet.text).to.be.a('string');
-    });
+    const search_word = "Development";
+    const number_of_tweets = 5;
+
+    const twitter_response = Twitter({
+        consumer_key: process.env.CONSUMER_KEY,
+        consumer_secret: process.env.CONSUMER_SECRET,
+        access_token_key: process.env.TOKEN_KEY,
+        access_token_secret: process.env.TOKEN_SECRET,
+      },
+      search_word,
+      number_of_tweets
+    );
+
+    twitter_response
+     .then((res) =>  {
+       expect(res.statusest).to.be.a('string');
+     })
+     .catch((err)  =>  {
+       console.log(err);
+     })
   });
 });
