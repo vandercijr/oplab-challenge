d06e241f083ff5b8535bad3c21d9f2f4789f5142
Vanderci <vandercijr@gmail.com>
2020-06-05 16:05:45 -0300

    added move adjustment function

--- src/algorithms/game/gamerule.js
+++ src/algorithms/game/gamerule.js
@@ -1,6 +1,20 @@
 'use strict';
 
 class GameRule {
+  // define valid moves, stones removal quantity
+  // considering this array in ascendent order for convinience
+  totalValidMoves = [2, 3, 5];
+  validMoves = [];
+
+  adjustValidMove(stones) {
+    this.validMoves = [];
+
+    while (GameOfStonesRule.validMoves[GameOfStonesRule.validMoves.length-1] > stones)
+      GameOfStonesRule.validMoves.splice(GameOfStonesRule.validMoves.length-1);
+
+
+    return true;
+  }
 }
 
 module.exports = GameRule;
