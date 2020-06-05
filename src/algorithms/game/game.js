ff07b9b117bf5b005596eccbc11c28bea1a52919
Vanderci <vandercijr@gmail.com>
2020-06-05 16:50:02 -0300

    added game main loop function

--- src/algorithms/game/game.js
+++ src/algorithms/game/game.js
@@ -12,6 +12,22 @@ const Player = require('./player');
 const events = require('events');
 
 const game = (stones) =>  {
+  const eventEmitter = new events.EventEmitter();
+  const p1 = new Player('First');
+  const p2 = new Player('Second');
+
+  const players = [];
+  let   turn = 1;
+
+  players.push(p1);
+  players.push(p2);
+
+  const nplayers = players.length;
+
+  const run = () => {
+    while (stones > 1) {
+    }
+  };
 };
 
 module.exports = game;
