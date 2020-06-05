cebaeb1a99f9cecf9e70aaf86887939c1e82b278
Vanderci <vandercijr@gmail.com>
2020-06-05 15:29:26 -0300

    added game implementation

---
+++ src/algorithms/game/game.js
@@ -0,0 +1,17 @@
+'use strict';
+/**
+ * Arquivo: gameofstones.js
+ * Author: Vanderci Curvelo Junior
+ * Description: implementation of gameofstones game challenge from https://www.hackerrank.com/challenges/game-of-stones-1/problem'
+ * port from mine C++ implementation
+ * run on nodejs
+ * Data: 05/06/2020
+ */
+const Player = require('./player');
+
+const events = require('events');
+
+const game = (stones) =>  {
+};
+
+module.exports = game;
