cc40842e0e19800c3bca5b5c2d96c6c102169580
Vanderci <vandercijr@gmail.com>
2020-06-05 16:02:37 -0300

    added player move function

--- src/algorithms/game/player.js
+++ src/algorithms/game/player.js
@@ -3,6 +3,22 @@
 const GameRule = require('./gamerule');
 
 class Player  {
+  constructor(name) {
+    this._name = name;
+    this.gameRule = new GameRule();
+  }
+
+  move(stones) {
+    if (stones === 1) return 0;
+
+    const valid_moves = this.gameRule.validMoves;
+
+    return valid_moves[Math.floor(Math.random() * valid_moves.length)];
+  }
+
+  get name() {
+    return this._name;
+  }
 }
 
 module.exports = Player;
