9f6c2387fcefd9b28e2ac80a72dbed563186267e
Vanderci <vandercijr@gmail.com>
2020-06-08 07:32:30 -0300

    fixed by eslint

--- src/algorithms/twitter/twitter.js
+++ src/algorithms/twitter/twitter.js
@@ -22,13 +22,13 @@ const tweetReader = (credentials, search_word, number_of_tweets) =>  {
     access_token_key: credentials.access_token_key,
     access_token_secret: credentials.access_token_secret,
     headers: {
-      "Authorization": "Bearer " + credentials.access_token_key
+      'Authorization': 'Bearer ' + credentials.access_token_key
     }
   });
 
   return  client.get('search/tweets', {q: encodeURI(search_word), count: number_of_tweets});
 
   // return  client.stream('statuses/filter', {filter_level: 'high', track: search_word});
-}
+};
 
 module.exports = tweetReader;
