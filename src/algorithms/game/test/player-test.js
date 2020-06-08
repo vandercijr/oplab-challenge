f224e1afc35d062f5f55721c809b5554f0a8540e
Vanderci <vandercijr@gmail.com>
2020-06-08 07:31:30 -0300

    fixed by eslint

--- src/algorithms/game/test/player-test.js
+++ src/algorithms/game/test/player-test.js
@@ -16,36 +16,36 @@ const player = new Player('A Player');
 
 const expect = chai.expect;
 
-describe('Test player object, according to challenge definition', ()  =>  {
-  it('Should return a player named A Player', ()  =>  {
+describe('Test player object, according to challenge definition', function() {
+  it('Should return a player named A Player', function() {
     expect(player.name).to.be.equal('A Player');
   });
 
-  it('Given 1 stones Should return 0 as a move', () =>  {
+  it('Given 1 stones Should return 0 as a move', function() {
     expect(player.move(1)).to.be.equal(0);
   });
 
-  it('Given 2 stones Should return 2 as a move', () =>  {
+  it('Given 2 stones Should return 2 as a move', function() {
     expect(player.move(2)).to.be.equal(2);
   });
 
-  it('Given 3 stones Should return 2 as a move', () =>  {
+  it('Given 3 stones Should return 2 as a move', function() {
     expect(player.move(3)).to.be.equal(2);
   });
 
-  it('Given 4 stones Should return 3 as a move', () =>  {
+  it('Given 4 stones Should return 3 as a move', function() {
     expect(player.move(4)).to.be.equal(3);
   });
 
-  it('Given 5 stones Should return 5 as a move', () =>  {
+  it('Given 5 stones Should return 5 as a move', function() {
     expect(player.move(5)).to.be.equal(5);
   });
 
-  it('Given 6 stones Should return 5 as a move', () =>  {
+  it('Given 6 stones Should return 5 as a move', function() {
     expect(player.move(6)).to.be.equal(5);
   });
 
-  it('Given 7 or more stones Should return 2,3 or 5 as a move', ()  =>  {
+  it('Given 7 or more stones Should return 2,3 or 5 as a move', function() {
     expect([2, 3, 5]).to.include(player.move(7));
     expect([2, 3, 5]).to.include(player.move(10));
     expect([2, 3, 5]).to.include(player.move(20));
