8a19b5dc4f07ac7c1d5abb7a4bc404d222696799
Vanderci <vandercijr@gmail.com>
2020-06-08 07:32:17 -0300

    fixed by eslint

--- src/algorithms/twitter/test/twitter-test.js
+++ src/algorithms/twitter/test/twitter-test.js
@@ -19,27 +19,27 @@ dotenv.config({path: path.resolve(__dirname + '/../../../../.env') });
 
 const expect = chai.expect;
 
-describe('Test tweets from twitter Api', () => {
-  it('Give a string search five tweets with this word', () =>  {
-    const search_word = "Development";
+describe('Test tweets from twitter Api', function() {
+  it('Give a string search five tweets with this word', function() {
+    const search_word = 'Development';
     const number_of_tweets = 5;
 
     const twitter_response = Twitter({
-        consumer_key: process.env.CONSUMER_KEY,
-        consumer_secret: process.env.CONSUMER_SECRET,
-        access_token_key: process.env.TOKEN_KEY,
-        access_token_secret: process.env.TOKEN_SECRET,
-      },
-      search_word,
-      number_of_tweets
+      consumer_key: process.env.CONSUMER_KEY,
+      consumer_secret: process.env.CONSUMER_SECRET,
+      access_token_key: process.env.TOKEN_KEY,
+      access_token_secret: process.env.TOKEN_SECRET,
+    },
+    search_word,
+    number_of_tweets
     );
 
     twitter_response
-     .then((res) =>  {
-       expect(res.statusest).to.be.a('string');
-     })
-     .catch((err)  =>  {
-       console.log(err);
-     })
+      .then((res) =>  {
+        expect(res.statusest).to.be.a('string');
+      })
+      .catch((err)  =>  {
+        console.log(err);
+      });
   });
 });
